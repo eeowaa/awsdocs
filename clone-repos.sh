e81cecfdcbf2336ada1871b3457288e103192250
eeowaa <7460185+eeowaa@users.noreply.github.com>
2021-06-11 11:54:09 -0600

    Add submodules

--- clone-repos.sh
+++ clone-repos.sh
@@ -2,7 +2,7 @@
 
 # Establish a temporary file to write to
 response=`mktemp`
-# trap "rm -f '$response'" 0 1 2 3 15
+trap "rm -f '$response'" 0 1 2 3 15
 
 # Until we run out of pages to retrieve...
 url='https://api.github.com/orgs/awsdocs/repos?per_page=100'
@@ -11,22 +11,29 @@ do
     {   # Query the GitHub API for a JSON array of repos (single page)
         curl --silent --include "$url"
     } | {
+        # Strip carriage returns from the response
+        tr -d '\r'
+    } | {
         # Write the response (including headers) to our temporary file
         tee "$response"
     } | {
         # Strip the headers from the response, leaving only JSON
         awk 'x{print}!$0{x=1}'
     } | {
-        # Print out the list of SSH URLs for cloning repos
-        jq '.[].ssh_url'
+        # Print out the list of remote URLs
+        jq -r '.[].clone_url'
     } | {
         # Clone the repos into subdirectories of the current directory
         while read repo
         do
-            git submodule add "$repo"
-            git submodule update --init --remote --recursive --depth 1
+            name=`echo "$repo" | sed -n 's#.*/\(.*\)\.git#\1#p'`
+            [ -e "$name/.git" ] || {
+                git submodule add "$repo"
+                git submodule update --init --remote --depth 1
+            }
         done
     }
     # Get the URL of the next page
     url=`sed -n 's/^link: <\(.*\)>; rel="next".*$/\1/p' "$response"`
+    echo >&2 "DEBUG: url = '$url'"
 done
